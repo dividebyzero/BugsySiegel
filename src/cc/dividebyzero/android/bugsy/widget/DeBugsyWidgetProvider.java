dcadbf4204cb850c4202dd550ff238cb0a0509c4
ronan 'zero' schwarz <ronan@novoda.com>
2013-09-27 16:27:55 +0200

    just some sketches

---
+++ src/cc/dividebyzero/android/bugsy/widget/DeBugsyWidgetProvider.java
@@ -0,0 +1,30 @@
+package cc.dividebyzero.android.bugsy.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+public class DeBugsyWidgetProvider extends AppWidgetProvider {
+
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+    }
+
+
+
+}
